8d1ed5a548768314f98d33c0254c3ad96db87f99
Keshav <keshav2552003@gmail.com>
2023-07-05 12:40:11 +0530

    Added life system and inproved UI

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ let ballHeight=13;
 let ballVelX=2;
 let ballVelY=1.3;
 let maxBallVelX=ballVelX+1;
-let maxBallVelY=ballVelY+1;
+let maxBallVelY=ballVelY+0.7;
 let minBallVelX=ballVelX-1;
-let minBallVelY=ballVelY-1;
+let minBallVelY=ballVelY-0.7;
 
 let ball={
-    x:(boardWidth+40)/2,
+    x:(boardWidth-ballWidth)/2,
     y: boardHeight/2,
     width: ballWidth,
     height: ballHeight,
@@ -49,6 +49,8 @@ let gameOver=false;
 let level=1;
 let ballCollidingWithPlayer = false;
 let lives = 3;
+let maxLives = 3;
+let gameStarted=false;
 
 let countdown = 3;
 let countdownActive = false;
@@ -56,13 +58,23 @@ let countdownActive = false;
 let leftArrowDown = false;
 let rightArrowDown = false;
 
+let heartImage = new Image();
+heartImage.src = './Assets/heart.png';
+
+let heartOutlineImage =new Image();
+heartOutlineImage.src="./Assets/heart-outline.png";
+
+
 document.addEventListener('keydown', function(e) {
     if (e.code === 'ArrowLeft') {
         leftArrowDown = true;
+        gameStarted = true;
     } else if (e.code === 'ArrowRight') {
         rightArrowDown = true;
+        gameStarted = true;
     }
 });
+
 
 document.addEventListener('keyup', function(e) {
     if (e.code === 'ArrowLeft') {
@@ -78,14 +90,14 @@ window.onload=function(){
     board.width=boardWidth;
     board.height=boardHeight;
     context=board.getContext("2d");
-    
+
     //blocks
     createBlocks();
 
     //player
     context.fillStyle="red";
     context.fillRect(player.x, player.y, playerWidth, playerHeight);
-    
+
     requestAnimationFrame(update);
     document.addEventListener("keydown", reset);
 } 
@@ -95,7 +107,7 @@ function update(){
     if(gameOver || countdownActive){
         return;
     }
-    
+
     context.clearRect(0,0,boardWidth,boardHeight)
 
     context.fillStyle="red";
@@ -106,14 +118,11 @@ function update(){
     context.fillStyle="white";
     context.shadowColor = "rgba(255,355,255, 0.7)";
     context.shadowBlur = 40;
-    ball.x += ball.velX;
-    ball.y += ball.velY;
+    if (gameStarted) {
+        ball.x += ball.velX;
+        ball.y += ball.velY;
+    }
     context.fillRect(ball.x, ball.y, ball.width, ball.height);
-
-
-    // log ball velocities after updating position
-    console.log("ball.velX after updating position:", ball.velX.toFixed(1));
-    console.log("ball.velY after updating position:", ball.velY.toFixed(1));
 
 
     // limit ball velocity to range between min and max values
@@ -129,43 +138,11 @@ function update(){
         ball.velY = minBallVelY * Math.sign(ball.velY);
     }
 
-    // log ball velocities after limiting maximum velocity
-    console.log("ball.velX after limiting maximum velocity:", ball.velX.toFixed(1));
-    console.log("ball.velY after limiting maximum velocity:", ball.velY.toFixed(1));
     //ball bounce off walls
-    if (ball.y <= 0 && ball.velY < 0) {
-        ball.velY *= -1;
-    } else if (ball.x <= 0 && ball.velX < 0) {
-        ball.velX *= -1;
-    } else if ((ball.x + ball.width) >= boardWidth && ball.velX > 0) {
-        ball.velX *= -1;
-    } else if (ball.y + ball.height >= boardHeight && ball.velY > 0) {
-        lives--; 
-        if (lives === 0) {
-            context.font = "20px sans-serif";
-            context.fillText("Gameover: Press 'space' to restart", 160, 400);
-            gameOver = true;
-        } else {
-            startCountdown();
-        }
-    }
+    ballBounceWall();
 
     //ball bounce off player paddle
-    if (!ballCollidingWithPlayer && (topCollision(ball, player) || bottomCollisions(ball, player))) {
-        let relativeMovement = getRelativeMovement();
-        ball.velY *= -1;
-        ball.velY += getRandomDeviation() - relativeMovement / 15;
-        ball.velX += getRandomDeviation() + relativeMovement / 15;
-        ballCollidingWithPlayer = true;
-    } else if (!ballCollidingWithPlayer && (leftCollision(ball, player) || rightCollision(ball, player))) {
-        let relativeMovement = getRelativeMovement();
-        ball.velX *= -1;
-        ball.velY += getRandomDeviation() - relativeMovement / 15;
-        ball.velX += getRandomDeviation() + relativeMovement / 15;
-        ballCollidingWithPlayer = true;
-    } else if (!topCollision(ball, player) && !bottomCollisions(ball, player) && !leftCollision(ball, player) && !rightCollision(ball, player)) {
-        ballCollidingWithPlayer = false;
-    }
+    ballBouncePaddle();
 
     //drawing blocks
     context.fillStyle="rgba(6, 0, 20,0.7)";
@@ -200,8 +177,13 @@ function update(){
         score+=100*blockRows*blockColumns;
         blockRows = Math.min(blockRows+1,blockMaxRows);
         level++;
+        playerWidth += 10; // increase paddle size
+        if (level % 3 === 0 && lives < maxLives) {
+            lives++; // gain an extra life
+        }
         createBlocks();
     }
+
 
     //score
     context.fillStyle="lightgreen";
@@ -212,7 +194,20 @@ function update(){
     context.fillText("Level: " + level, boardWidth - 80, 25);
 
     //lives
-    context.fillText("Lives: " + lives, boardWidth/2 - 40, 25);
+    context.shadowColor = "rgba(255,0,0, 0.5)";
+    context.shadowBlur = 10;
+    for (let i = 0; i < 3; i++) {
+        let x = boardWidth/2 - 40 + i * 30;
+        let y = 10;
+        let width = 20;
+        let height = 20;
+        if (i < lives) {
+            context.drawImage(heartImage, x, y, width, height);
+        } else {
+            context.drawImage(heartOutlineImage, x, y, width, height);
+        }
+    }
+
 
     //player move
     if (leftArrowDown) {
@@ -293,6 +288,43 @@ function createBlocks(){
         }
     }
     blockCount=blockArr.length;
+}
+
+function ballBounceWall(){
+    if (ball.y <= 0 && ball.velY < 0) {
+        ball.velY *= -1;
+    } else if (ball.x <= 0 && ball.velX < 0) {
+        ball.velX *= -1;
+    } else if ((ball.x + ball.width) >= boardWidth && ball.velX > 0) {
+        ball.velX *= -1;
+    } else if (ball.y + ball.height >= boardHeight && ball.velY > 0) {
+        lives--; 
+        if (lives === 0) {
+            context.font = "20px sans-serif";
+            context.fillText("Gameover: Press 'space' to restart", 160, 400);
+            gameOver = true;
+        } else {
+            startCountdown();
+        }
+    }
+}
+
+function ballBouncePaddle(){
+    if (!ballCollidingWithPlayer && (topCollision(ball, player) || bottomCollisions(ball, player))) {
+        let relativeMovement = getRelativeMovement();
+        ball.velY *= -1;
+        ball.velY += getRandomDeviation() - relativeMovement / 15;
+        ball.velX += getRandomDeviation() + relativeMovement / 15;
+        ballCollidingWithPlayer = true;
+    } else if (!ballCollidingWithPlayer && (leftCollision(ball, player) || rightCollision(ball, player))) {
+        let relativeMovement = getRelativeMovement();
+        ball.velX *= -1;
+        ball.velY += getRandomDeviation() - relativeMovement / 15;
+        ball.velX += getRandomDeviation() + relativeMovement / 15;
+        ballCollidingWithPlayer = true;
+    } else if (!topCollision(ball, player) && !bottomCollisions(ball, player) && !leftCollision(ball, player) && !rightCollision(ball, player)) {
+        ballCollidingWithPlayer = false;
+    }
 }
 
 function resetGame(){
@@ -387,6 +419,19 @@ function drawGameObjects() {
     context.fillText("Level: " + level, boardWidth - 80, 25);
 
     //lives
-    context.fillText("Lives: " + lives, boardWidth/2 - 40, 25);
+    context.shadowColor = "rgba(255,0,0, 0.5)";
+    context.shadowBlur = 10;
+    for (let i = 0; i < 3; i++) {
+        let x = boardWidth/2 - 40 + i * 30;
+        let y = 10;
+        let width = 20;
+        let height = 20;
+        if (i < lives) {
+            context.drawImage(heartImage, x, y, width, height);
+        } else {
+            context.drawImage(heartOutlineImage, x, y, width, height);
+        }
+    }
+
 
 }
